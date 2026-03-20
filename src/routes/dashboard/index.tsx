39695125c3be4f31659b2354e0617810d14c6b9e
Maria Leticia <mleticiagaspar17@gmail.com>
2026-03-20 18:56:33 -0300

    feat: add authenticated dashboard with user greeting

--- src/routes/dashboard/index.tsx
+++ src/routes/dashboard/index.tsx
@@ -1,3 +1,4 @@
+import { useAuth } from "@/hooks/use-auth";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/dashboard/")({
@@ -5,5 +6,22 @@ export const Route = createFileRoute("/dashboard/")({
 });
 
 function RouteComponent() {
-  return <div>Hello "/dashboard/"!</div>;
+  const { loggedUser } = useAuth();
+
+  return (
+    <main className="flex-1 flex flex-col items-center justify-center pb-20 px-6 bg-[#FAF9F6]">
+      <div className="space-y-2 text-center">
+        <span className="text-[10px] tracking-[0.4em] uppercase text-[#D6453D] font-bold">
+          Conta Autenticada
+        </span>
+        <h1 className="text-5xl md:text-6xl font-extralight tracking-tighter text-stone-900">
+          Olá,{" "}
+          <span className="italic font-serif text-[#D6453D]">
+            {loggedUser?.firstName}
+          </span>{" "}
+          .
+        </h1>
+      </div>
+    </main>
+  );
 }
